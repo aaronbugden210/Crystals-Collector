b739a2338d192ee658003c1cd251943651c0f516
Aaron Bugden <aaronbugden@gmail.com>
2019-05-21 16:06:26 -0400

    Updates to code to make it object oriented

--- assets/game.js
+++ assets/game.js
@@ -1,10 +1,33 @@
-//The first four variables gives the gems a random number
-//compNum is the random number that the computer generates
+//Object initialization to be used for multiple Gem objects in the game
+var Gem = 
+{
+	value: this.Math.ceil(Math.random()*12),
+	title: null,
+	resetValue: function()
+	{
+		this.value = Math.ceil(Math.random()*12);
+	}
+};
 
-var redGem = Math.ceil(Math.random()*12);
-var greenGem = Math.ceil(Math.random()*12);
-var blueGem = Math.ceil(Math.random()*12);
-var purpleGem = Math.ceil(Math.random()*12);
+var redGem = Object.create(Gem);
+redGem.title = $("#redGem").attr("title", "Value: " + redGem.value);
+
+var greenGem = Object.create(Gem);
+greenGem.title = $("#greenGem").attr("title", "Value: " + greenGem.value);
+
+var blueGem = Object.create(Gem);
+blueGem.title = $("#blueGem").attr("title", "Value: " + blueGem.value);
+
+var purpleGem = Object.create(Gem);
+purpleGem.title = $("#purpleGem").attr("title", "Value: " + purpleGem.value);
+
+console.log("Red gem = " + redGem.value);
+console.log("Green gem = " + greenGem.value);
+console.log("Blue Gem = " + blueGem.value);
+console.log("Purple Gem = " + purpleGem.value);
+console.log("Comp Num = " + compNum);
+
+checkValues(redGem, greenGem, blueGem, purpleGem);
 
 var compNum = Math.ceil(Math.random() * ((120 - 19) + 1) + 19);
 
@@ -15,12 +38,6 @@ var score = 0;
 var wins = 0;
 var loses = 0;
 
-// console.log("Red gem = " + redGem);
-// console.log("Green gem = " + greenGem);
-// console.log("Blue Gem = " + blueGem);
-// console.log("Purple Gem = " + purpleGem);
-// console.log("Comp Num = " + compNum);
-
 //Sends value of compNum to the div that displays the value to the player
 $("#goal").text("Goal: " + compNum);
 
@@ -28,21 +45,62 @@ $("#goal").text("Goal: " + compNum);
 $("#wins").text("Wins: " + wins);
 $("#loses").text("Loses: " + loses);
 
+//Function that resets the whole game to zero
+//The object type 'Gem' has a reset function (resetValue()) exclusive to the object itself
 function reset()
 {
-	redGem = Math.ceil(Math.random()*12);
-	greenGem = Math.ceil(Math.random()*12);
-	blueGem = Math.ceil(Math.random()*12);
-	purpleGem = Math.ceil(Math.random()*12);
+	redGem.resetValue();
+	greenGem.resetValue();
+	blueGem.resetValue();
+	purpleGem.resetValue();
 	compNum = Math.ceil(Math.random()*120);
 	score = 0;
 
-	// console.log("Red gem = " + redGem);
-	// console.log("Green gem = " + greenGem);
-	// console.log("Blue Gem = " + blueGem);
-	// console.log("Purple Gem = " + purpleGem);
+	console.log("New Red gem = " + redGem.value);
+	console.log("New Green gem = " + greenGem.value);
+	console.log("New Blue Gem = " + blueGem.value);
+	console.log("New Purple Gem = " + purpleGem.value);
 
-	// console.log("Comp Num = " + compNum);
+	checkValues(redGem, greenGem, blueGem, purpleGem);
+
+	redGem.title = $("#redGem").attr("title", "Value: " + redGem.value);
+	greenGem.title = $("#greenGem").attr("title", "Value: " + greenGem.value);
+	blueGem.title = $("#blueGem").attr("title", "Value: " + blueGem.value);
+	purpleGem.title = $("#purpleGem").attr("title", "Value: " + purpleGem.value);
+
+	console.log("New Comp Num = " + compNum);
+}
+
+//Function that checks values of the Gem objects and resets them if they are equal to another Gem objects value
+//Since the first gem will have the first gem (w) is the first one to gain a value,
+//I chose to reset any value other than (w)
+function checkValues(w, x, y, z)
+{
+	do
+	{
+		if(x.value === w.value || x.value === y.value || x.value === z.value)
+		{
+			x.resetValue();
+			x.title = $("#greenGem").attr("title", "Value: " + x.value);
+			console.log("Check values green: " + x.value);
+		}
+
+		else if(y.value === w.value || y.value === x.value || y.value === z.value)
+		{
+			y.resetValue();
+			y.title = $("#blueGem").attr("title", "Value: " + y.value);
+			console.log("Check values blue: " + y.value);
+		}
+
+		else if(z.value === w.value || z.value === x.value || z.value === y.value)
+		{
+			z.resetValue();
+			z.title = $("#purpleGem").attr("title", "Value: " + z.value);
+			console.log("Check values purple: " + z.value);
+		}
+	}while(x.value === w.value || x.value === y.value || x.value === z.value ||
+		y.value === w.value || y.value === x.value || y.value === z.value ||
+		z.value === w.value || z.value === x.value || z.value === y.value);
 }
 
 function endGame()
@@ -78,28 +136,28 @@ $(document).ready(function()
 {
 	$("#redGem").on("click", function()
 	{
-		score += redGem;
+		score += redGem.value;
 		$("#actScore").text("Your score is: " + score);
 		endGame();
 	});
 
 	$("#greenGem").on("click", function()
 	{
-		score += greenGem;
+		score += greenGem.value;
 		$("#actScore").text("Your score is: " + score);
 		endGame();
 	});
 
 	$("#blueGem").on("click", function()
 	{
-		score += blueGem;
+		score += blueGem.value;
 		$("#actScore").text("Your score is: " + score);
 		endGame();
 	});
 
 	$("#purpleGem").on("click", function()
 	{
-		score += purpleGem;
+		score += purpleGem.value;
 		$("#actScore").text("Your score is: " + score);
 		endGame();
 	});
